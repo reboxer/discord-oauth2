3c63bb23fdea5e4fd8cb852f8724e31cc8fe103e
santiago <7484116+reboxer@users.noreply.github.com>
2019-11-16 21:19:21 -0300

    fix typings

--- index.d.ts
+++ index.d.ts
@@ -1,76 +1,85 @@
-declare module "discord-oauth2" {
-    interface User {
-        id: string,
-        avatar: string | null | undefined,
-        username: string,
-        discriminator: string,
+interface User {
+    id: string,
+    avatar: string | null | undefined,
+    username: string,
+    discriminator: string,
 
-        bot?: boolean,
-        email?: string,
-        flags?: number,
-        locale?: string,
-        verified?: boolean,
-        mfa_enabled?: string,
-        premium_type?: number
-    }
-
-    interface Integration {
-        id: string,
-        user: User,
-        name: string,
-        type: string,
-        account: {
-            id: string,
-            name: string
-        }
-        enabled: boolean,
-        role_id: string,
-        syncing: boolean,
-        synced_at: string,
-        expire_behavior: number,
-        expire_grace_period: number
-    }
-
-    interface Connection {
-        id: string,
-        type: string,
-        name: string,
-        revoked?: string,
-        verified: string,
-        visibility: string,
-        friend_sync: boolean,
-        show_activity: boolean,
-        integrations?: Integration[]
-    }
-
-    export class OAuth2 {
-        constructor(options?: {
-            requestTimeout?: number,
-            latencyThreshold?: number,
-            ratelimiterOffset?: number
-        });
-        tokenRequest(object: {
-            code: string,
-            scope: string,
-            client_id: string,
-            grant_type: string,
-            redirect_uri: string,
-            client_secret: string
-        }): Promise<any>;
-        revokeToken(access_token: string, credentials: string): Promise<string>;
-        getUser(access_token: string): Promise<User>;
-        getUserGuilds(access_token: string): Promise<Object[]>;
-        getUserConnections(access_token: string): Promise<Connection[]>;
-        addMember(data: {
-            deaf?: boolean,
-            mute?: boolean,
-            roles?: string[],
-            nickname?: string,
-
-            userId: string,
-            guildId: string,
-            botToken: string,
-            accessToken: string
-        }): Promise<any>;
-    }
+    bot?: boolean,
+    email?: string,
+    flags?: number,
+    locale?: string,
+    verified?: boolean,
+    mfa_enabled?: string,
+    premium_type?: number
 }
+
+interface Integration {
+    id: string,
+    user: User,
+    name: string,
+    type: string,
+    account: {
+        id: string,
+        name: string
+    }
+    enabled: boolean,
+    role_id: string,
+    syncing: boolean,
+    synced_at: string,
+    expire_behavior: number,
+    expire_grace_period: number
+}
+
+interface Connection {
+    id: string,
+    type: string,
+    name: string,
+    revoked?: string,
+    verified: string,
+    visibility: string,
+    friend_sync: boolean,
+    show_activity: boolean,
+    integrations?: Integration[]
+}
+
+interface TokenRequestResult {
+    access_token: string,
+    token_type: string,
+    expires_in: number,
+    refresh_token: string,
+    scope: string
+}
+
+declare class OAuth {
+    constructor(options?: {
+        credentials?: string,
+        requestTimeout?: number,
+        latencyThreshold?: number,
+        ratelimiterOffset?: number
+    });
+    tokenRequest(object: {
+        code: string,
+        scope: string,
+        client_id: string,
+        grant_type: string,
+        redirect_uri: string,
+        client_secret: string
+    }): Promise<TokenRequestResult>;
+    revokeToken(access_token: string, credentials: string): Promise<string>;
+    getUser(access_token: string): Promise<User>;
+    getUserGuilds(access_token: string): Promise<object[]>;
+    getUserConnections(access_token: string): Promise<Connection[]>;
+    addMember(data: {
+        deaf?: boolean,
+        mute?: boolean,
+        roles?: string[],
+        nickname?: string,
+
+        userId: string,
+        guildId: string,
+        botToken: string,
+        accessToken: string
+    }): Promise<object | string>;
+}
+
+export = OAuth;

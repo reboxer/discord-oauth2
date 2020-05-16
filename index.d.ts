d060baed950e35ead6ecbec1c29b009326ba5528
santiago <7484116+reboxer@users.noreply.github.com>
2020-05-16 19:49:22 -0300

    use semicolon instead of coma

--- index.d.ts
+++ index.d.ts
@@ -1,74 +1,73 @@
 import { EventEmitter } from "events";
 
 interface User {
-    id: string,
-    avatar: string | null | undefined,
-    username: string,
-    discriminator: string,
-
-    bot?: boolean,
-    email?: string,
-    flags?: number,
-    locale?: string,
-    verified?: boolean,
-    mfa_enabled?: string,
-    premium_type?: number,
+    id: string;
+    avatar: string | null | undefined;
+    username: string;
+    discriminator: string;
+    bot?: boolean;
+    email?: string;
+    flags?: number;
+    locale?: string;
+    verified?: boolean;
+    mfa_enabled?: string;
+    premium_type?: number;
 }
 
 interface Member {
-    nick: string | null | undefined,
-    user: User,
-    deaf: boolean,
-    mute: boolean,
-    roles: string[],
-    joined_at: number,
-    premium_since: number | null | undefined,
+    nick: string | null | undefined;
+    user: User;
+    deaf: boolean;
+    mute: boolean;
+    roles: string[];
+    joined_at: number;
+    premium_since: number | null | undefined;
 }
 
 interface Integration {
-    id: string,
-    user: User,
-    name: string,
-    type: string,
+    id: string;
+    user: User;
+    name: string;
+    type: string;
     account: {
-        id: string,
-        name: string,
-    },
-    enabled: boolean,
-    role_id: string,
-    syncing: boolean,
-    synced_at: string,
-    expire_behavior: number,
-    expire_grace_period: number,
+        id: string;
+        name: string;
+    };
+    enabled: boolean;
+    role_id: string;
+    syncing: boolean;
+    synced_at: string;
+    expire_behavior: number;
+    expire_grace_period: number;
 }
 
 interface Connection {
-    id: string,
-    type: string,
-    name: string,
-    revoked?: string,
-    verified: string,
-    visibility: string,
-    friend_sync: boolean,
-    show_activity: boolean,
-    integrations?: Integration[],
+    id: string;
+    type: string;
+    name: string;
+    revoked?: string;
+    verified: string;
+    visibility: string;
+    friend_sync: boolean;
+    show_activity: boolean;
+    integrations?: Integration[];
 }
 
 interface TokenRequestResult {
-    access_token: string,
-    token_type: string,
-    expires_in: number,
-    refresh_token: string,
-    scope: string,
+    access_token: string;
+    token_type: string;
+    expires_in: number;
+    refresh_token: string;
+    scope: string;
 }
 
 interface PartialGuild {
-    id: string,
-    name: string,
-    icon: string | null | undefined,
-    owner: boolean,
-    features: string[],
-    permissions?: number,
+    id: string;
+    name: string;
+    icon: string | null | undefined;
+    owner: boolean;
+    features: string[];
+    permissions?: number;
 }
 
 declare class OAuth extends EventEmitter {
@@ -100,7 +99,6 @@ declare class OAuth extends EventEmitter {
         mute?: boolean,
         roles?: string[],
         nickname?: string,
-
         userId: string,
         guildId: string,
         botToken: string,
